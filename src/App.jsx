e2e7e2e39fb6267e678ce225668345f9cdfffea3
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2025-12-06 14:07:14 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { Route,  Routes } from "react-router-dom"
+import { Header } from "./Components/Header"
+import { Home } from "./Pages/Home"
+import { About } from "./Pages/About"
+import { Contact } from "./Pages/Contact"
+import { Cart } from "./Pages/Cart"
+
 const App = () => {
   return (
-    <h2>Hello World</h2>
+  <div>
+     <Header />
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/cart" element={<Cart />} />
+     </Routes>
+  </div>
+   
   )
 }
 
